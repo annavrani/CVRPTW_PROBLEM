5897d22efb6f42afcd62e28ee01ff85fa2d9f5d8
annavrani <157830638+annavrani@users.noreply.github.com>
2024-03-21 20:22:13 +0200

    Google Example with Unload
    VRPTW with data from google OR tools adding also unload times for the vehicles

---
+++ googleexample.py
@@ -0,0 +1,139 @@
+"""Vehicles Routing Problem (VRP) with Time Windows."""
+
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+
+
+def create_data_model():
+    """Stores the data for the problem."""
+    data = {}
+    data["time_matrix"] = [
+        [0, 35, 38, 32, 32, 43, 29],
+        [36, 0, 11, 4, 10, 18, 9],
+        [39, 10, 0, 11, 13, 18, 14],
+        [32, 4, 11, 0, 6, 16, 7],
+        [32, 9, 13, 6, 0, 18, 7],
+        [45, 19, 18, 18, 19, 0, 20],
+        [32, 11, 15, 8, 8, 20, 0]
+    ]
+    data["unload_times"] = [0, 10, 10, 5, 5, 15, 10]
+    data["time_windows"] = [
+        (0, 5),  # depot
+        (0, 80),  # 1
+        (0, 120),  # 2
+        (0, 120),  # 3
+        (0, 120),  # 4
+        (0, 120),  # 5
+        (0, 40),  # 6
+    ]
+    data["num_vehicles"] = 2
+    data["depot"] = 0
+    return data
+
+
+def print_solution(data, manager, routing, solution):
+    """Prints solution on console."""
+    print(f"Objective: {solution.ObjectiveValue()}")
+    time_dimension = routing.GetDimensionOrDie("Time")
+    total_time = 0
+    for vehicle_id in range(data["num_vehicles"]):
+        index = routing.Start(vehicle_id)
+        plan_output = f"Route for vehicle {vehicle_id}:\n"
+        while not routing.IsEnd(index):
+            time_var = time_dimension.CumulVar(index)
+            plan_output += (
+                f"{manager.IndexToNode(index)}"
+                f" Time({solution.Min(time_var)},{solution.Max(time_var)})"
+                " -> "
+            )
+            index = solution.Value(routing.NextVar(index))
+        time_var = time_dimension.CumulVar(index)
+        plan_output += (
+            f"{manager.IndexToNode(index)}"
+            f" Time({solution.Min(time_var)},{solution.Max(time_var)})\n"
+        )
+        plan_output += f"Time of the route: {solution.Min(time_var)}min\n"
+        print(plan_output)
+        total_time += solution.Min(time_var)
+    print(f"Total time of all routes: {total_time}min")
+
+
+def main():
+    """Solve the VRP with time windows."""
+    # Instantiate the data problem.
+    data = create_data_model()
+
+    # Create the routing index manager.
+    manager = pywrapcp.RoutingIndexManager(
+        len(data["time_matrix"]), data["num_vehicles"], data["depot"]
+    )
+
+    # Create Routing Model.
+    routing = pywrapcp.RoutingModel(manager)
+
+    # Create and register a transit callback.
+    def time_callback(from_index, to_index):
+        """Returns the travel time between the two nodes."""
+        # Convert from routing variable Index to time matrix NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data["time_matrix"][from_node][to_node] + data["unload_times"][from_node]
+
+
+    transit_callback_index = routing.RegisterTransitCallback(time_callback)
+
+    # Define cost of each arc.
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+
+    # Add Time Windows constraint.
+    time = "Time"
+    routing.AddDimension(
+        transit_callback_index,
+        150,  # allow waiting time
+        150,  # maximum time per vehicle
+        False,  # Don't force start cumul to zero.
+        time,
+    )
+    time_dimension = routing.GetDimensionOrDie(time)
+    # Add time window constraints for each location except depot.
+    for location_idx, time_window in enumerate(data["time_windows"]):
+        if location_idx == data["depot"]:
+            continue
+        index = manager.NodeToIndex(location_idx)
+        time_dimension.CumulVar(index).SetRange(time_window[0], time_window[1])
+    # Add time window constraints for each vehicle start node.
+    depot_idx = data["depot"]
+    for vehicle_id in range(data["num_vehicles"]):
+        index = routing.Start(vehicle_id)
+        time_dimension.CumulVar(index).SetRange(
+            data["time_windows"][depot_idx][0], data["time_windows"][depot_idx][1]
+        )
+
+    # Instantiate route start and end times to produce feasible times.
+    for i in range(data["num_vehicles"]):
+        routing.AddVariableMinimizedByFinalizer(
+            time_dimension.CumulVar(routing.Start(i))
+        )
+        routing.AddVariableMinimizedByFinalizer(time_dimension.CumulVar(routing.End(i)))
+
+    # Setting first solution heuristic.
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+    )
+   # search_parameters.local_search_metaheuristic = (
+   #     routing_enums_pb2.LocalSearchMetaheuristic.AUTOMATIC
+   # )
+   # search_parameters.time_limit.seconds = 15
+
+    # Solve the problem.
+    solution = routing.SolveWithParameters(search_parameters)
+
+    # Print solution on console.
+    if solution:
+        print_solution(data, manager, routing, solution)
+
+
+if __name__ == "__main__":
+    main()

691f73f85d663aea5d28a1efe452be50d0491f39
annavrani <157830638+annavrani@users.noreply.github.com>
2024-03-30 21:12:41 +0200

    output excel added
    Output excel - routes and duration

--- mycode.py
+++ mycode.py
@@ -4,7 +4,7 @@ from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 import requests
 import pandas as pd
-
+import xlsxwriter
 
 # Set pandas display options to show all columns and rows without truncation
 pd.set_option('display.max_columns', None)
@@ -12,85 +12,38 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
 
-# Your DataFrame creation code
 
-# Define your list of locations (origins and destinations)
-locations = [
-    "26 Anatolikis Romilias, Thessaloniki, 54454, Greece",
-    "3 Kolchidos, Kalamaria, 55131, Greece",
-    "2 Markou Mpotsari, Thessaloniki, 54643, Greece",
-    "10 Alonnisou, Thessaloniki, 54638, Greece",
-    "7 Dimokratias, Panorama, 55236, Greece",
-    "30 Andrea Papandreou, Kalamaria, 55133, Greece",
-    "45 Tsimiski, Thessaloniki, 54623, Greece ",
-    "3 Aggelon, Pefka, 57010, Greece",
-    "46 Steliou Kazantzidi, Thermi, 57001, Greece",
-    "55 Egnatia, Thessaloniki, 54631, Greece",
-    "15 Nikolaou Plastira, Efkarpia, 56429, Greece ",
-    "10 Kolokotroni, Evosmos, 56224, Greece "
-]
-
-# API key for Distance Matrix.AI API
-API_KEY = '4gjG1bO9H3SylqgStT3icoUbOdYAFfSOi4AKXmNAakoE1LRIBor5j2M7O2mq2HG2'
-
-# Initialize an empty DataFrame for the distance matrix
-distance_matrix_df = pd.DataFrame(index=locations, columns=locations)
-
-# Iterate through all pairs of locations
-for origin in locations:
-    for destination in locations:
-        # Make a request to the Distance Matrix.AI API to get the duration
-        url = f"https://api.distancematrix.ai/maps/api/distancematrix/json?origins={origin}&destinations={destination}&key={API_KEY}"
-        response = requests.get(url)
-        data = response.json()
-
-        # Parse the response and extract the duration information
-        duration = None
-        if 'rows' in data and len(data['rows']) > 0:
-            row = data['rows'][0]
-            if 'elements' in row and len(row['elements']) > 0:
-                element = row['elements'][0]
-                if 'duration' in element:
-                    duration = element['duration']['value'] / 60  # Convert duration from seconds to minutes
-
-        # Populate the matrix DataFrame with the extracted durations
-        distance_matrix_df.at[origin, destination] = round(duration)
-
-# Display the distance matrix DataFrame
-print("Distance Matrix DataFrame:")
-print(distance_matrix_df)
-
-distance_matrix_df.to_csv('distance_matrix.csv', index=True)
+# Read the Excel file into a pandas DataFrame
+df = pd.read_excel("C:/Users/annav/OneDrive/Υπολογιστής/DATA XRONOI/data_xronoi_best_guess_ABCD.xlsx", index_col=0)
+df2 = pd.read_excel("C:/Users/annav/PycharmProjects/CVRPTW_PROBLEM/timewindows.xlsx")
 
 def create_data_model():
     """Stores the data for the problem."""
     data = {}
-    data["time_matrix"] = distance_matrix_df.values.tolist()
-    data["time_windows"] = [
-        (0, 100),  # Depot
-        (25, 40),  # Loc1
-        (25, 55),  # Loc2
-        (80, 100),  # Loc3
-        (0, 20),  # Loc4
-        (50, 70),  # Loc5
-        (30, 100),  # Loc6
-        (25, 45),  # Loc7
-        (35, 70),  # Loc8
-        (60, 100),  # Loc9
-        (0, 35),  # Loc10
-        (50, 70),  # Loc11
-    ]
-    data["num_vehicles"] = 2
+    data["locations"] = 79
+    data["time_matrix"] = df.values.tolist()
+    data["unload_times"] = df2["Unload Times"][0:data["locations"]].astype(int).tolist()
+    data["earliest_arrival_time"] = df2["EAT"][0:data["locations"]].astype(int).tolist()
+    data["latest_arrival_time"] = df2["LAT"][0:data["locations"]].astype(int).tolist()
+    data["time_windows"] = list(zip(data["earliest_arrival_time"], data["latest_arrival_time"]))
+    data["num_vehicles"] = 4
     data["depot"] = 0
     return data
 
+# Call create_data_model function and print the result
+print(create_data_model())
 
 def print_solution(data, manager, routing, solution):
+
+    solutionDF = pd.DataFrame(columns=['Route', 'Time', 'Google Maps Link'])
     """Prints solution on console."""
     print(f"Objective: {solution.ObjectiveValue()}")
     time_dimension = routing.GetDimensionOrDie("Time")
     total_time = 0
+    dictObj = {}
+
     for vehicle_id in range(data["num_vehicles"]):
+        dictObj = {}
         index = routing.Start(vehicle_id)
         plan_output = f"Route for vehicle {vehicle_id}:\n"
         while not routing.IsEnd(index):
@@ -106,11 +59,25 @@ def print_solution(data, manager, routing, solution):
             f"{manager.IndexToNode(index)}"
             f" Time({solution.Min(time_var)},{solution.Max(time_var)})\n"
         )
-        plan_output += f"Time of the route: {solution.Min(time_var)}min\n"
+        routeInfo = plan_output
+        timeInfo = solution.Min(time_var)
+        plan_output += f"Time of the route: {timeInfo}min\n"
+        dictObj['Time'] = timeInfo
+        dictObj['Route'] = routeInfo
+        solutionDF.loc[len(solutionDF)] = dictObj
         print(plan_output)
         total_time += solution.Min(time_var)
     print(f"Total time of all routes: {total_time}min")
+    writer = pd.ExcelWriter('output.xlsx', engine='xlsxwriter')
 
+    # Get the xlsxwriter workbook and worksheet objects.
+    workbook = writer.book
+    firstSheetName = 'RoutingOptimization'
+    solutionDF.to_excel(writer, sheet_name=firstSheetName, index=False)
+
+    worksheet = writer.sheets[firstSheetName]
+
+    writer.close()
 
 def main():
     """Solve the VRP with time windows."""
@@ -131,23 +98,27 @@ def main():
         # Convert from routing variable Index to time matrix NodeIndex.
         from_node = manager.IndexToNode(from_index)
         to_node = manager.IndexToNode(to_index)
-        return data["time_matrix"][from_node][to_node]
+        return data["time_matrix"][from_node][to_node] + data["unload_times"][from_node]
+
 
     transit_callback_index = routing.RegisterTransitCallback(time_callback)
 
     # Define cost of each arc.
     routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
 
+
     # Add Time Windows constraint.
     time = "Time"
     routing.AddDimension(
         transit_callback_index,
-        30,  # allow waiting time
-        100,  # maximum time per vehicle
+        15,  # allow waiting time
+        420,  # maximum time per vehicle
         False,  # Don't force start cumul to zero.
         time,
     )
     time_dimension = routing.GetDimensionOrDie(time)
+
+
     # Add time window constraints for each location except depot.
     for location_idx, time_window in enumerate(data["time_windows"]):
         if location_idx == data["depot"]:
@@ -169,11 +140,15 @@ def main():
         )
         routing.AddVariableMinimizedByFinalizer(time_dimension.CumulVar(routing.End(i)))
 
-    # Setting first solution heuristic.
-    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-    search_parameters.first_solution_strategy = (
-        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
-    )
+        # Setting first solution heuristic.
+        search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+        search_parameters.first_solution_strategy = (
+            routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+        )
+        search_parameters.local_search_metaheuristic = (
+            routing_enums_pb2.LocalSearchMetaheuristic.TABU_SEARCH
+        )
+        search_parameters.time_limit.seconds = 20
 
     # Solve the problem.
     solution = routing.SolveWithParameters(search_parameters)
